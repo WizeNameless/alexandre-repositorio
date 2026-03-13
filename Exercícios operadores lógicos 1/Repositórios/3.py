3f3558aaf330ac48920e43721ba80808649b7de0
Alexandre <alexandresescoto@gmail.com>
2026-03-13 20:54:02 -0300

    correção das atvd

--- Exercícios operadores lógicos 1/Repositórios/3.py
+++ Exercícios operadores lógicos 1/Repositórios/3.py
@@ -1,1 +1,6 @@
 # Pelo menos um número maior que 50
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+print(n1 > 50 or n2 > 50)
+
--- Exercícios operadores lógicos 1/Repositórios/4.py
+++ Exercícios operadores lógicos 1/Repositórios/4.py
@@ -1,1 +1,3 @@
 # Número NÃO é zero
+n = float(input("Digite um número: "))
+print(n != 0)
