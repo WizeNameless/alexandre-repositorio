20c26057ba6a9e18da52740f148601b6b3c3bd4b
alexandre <alexandresescoto@gmail.com>
2026-03-17 20:59:10 -0300

    Lista de compras e Numeros

---
+++ Exercícios de fixação - Listas, Tuplas e Dicionários/Lista de Numeros.py
@@ -0,0 +1,14 @@
+lista_numeros = [10, 20, 30, 40, 50]
+print ("Os números da lista são:", lista_numeros)
+lista_numeros.append (60)
+print ("Após adicionar um número:", lista_numeros)
+lista_numeros.remove (30)
+print ("Após remover um número:", lista_numeros)
+lista_numeros.insert (0, 5)
+lista_numeros.insert (3, 25)
+lista_numeros.insert (2, 15)
+print ("Após inserir uns número:", lista_numeros)
+print ("Tem o número 15 na lista?", 15 in lista_numeros)
+print ("Tamanho da lista:", len (lista_numeros))
+
+#gg
---
+++ Exercícios de fixação - Listas, Tuplas e Dicionários/Lista de compras.py
@@ -0,0 +1,36 @@
+lista_compras = ["maçã", "banana", "laranja", "pão", "leite"]
+print (lista_compras)
+
+# Acessando itens da lista
+print ("Primeito item da lista:", lista_compras [0])   #maçã
+print ("Último item da lista:", lista_compras [-1])    #leite
+
+# Alterando itens da lista
+lista_compras [1] = "uva"
+print ("Após a alteração:", lista_compras)
+
+# Adicionando itens à lista
+lista_compras.append ("fermento")           # Adiciona "fermento" ao final da lista
+lista_compras.append ("açúcar")             # Adiciona "açúcar" ao final da lista
+lista_compras.insert (2, "manteiga")        # Adicionando "manteiga" na posição 2 (entre "uva" e "laranja")
+print ("Após adicionar itens:", lista_compras)
+
+# Removendo itens da lista
+lista_compras.remove ("fermento")                # Remove "fermento" da lista
+ultimo = lista_compras.pop()                     # Remove e retorna o último item da lista (açúcar)   [nao entendi]
+print ("Após remover o fermento:", lista_compras, "| Último da lista é:", ultimo)
+
+# Lista Atualizada
+print ("Lista de compras atualizada:", lista_compras)
+
+# Verificando se um item está na lista
+print ("Laranja está na lista?", "laranja" in lista_compras)
+
+# Tamanho da lista
+print ("Número de itens na lista:", len (lista_compras))
+
+# Exibir itens da lista usando fatiamento (slicing)
+print ("Primeiros 3 itens da lista:", lista_compras [0:3])   # Exibe os itens do índice 0 ao 2
+print ("Últimos 2 itens da lista:", lista_compras [-2:])     # Exibe os últimos 2 itens da lista
+print ("Itens do meio da lista:", lista_compras [1:4])       # Exibe os itens do índice 1 ao 3
+
