6544baea783dc9a64833d9507013a1cef8823d7a
alexandre <alexandresescoto@gmail.com>
2026-03-19 20:55:45 -0300

    att dos exercicios de lista, tuplas e dic

--- Exercícios operadores de atribuição/Exercícios operadores de atribuição/1.py
+++ Exercícios operadores de atribuição/Exercícios operadores de atribuição/1.py
@@ -1,1 +1,5 @@
 # Leia saldo (float) e depósito (float). Use saldo += deposito e mostre o novo saldo.
+saldo = float(input("Digite o saldo: "))
+deposito = float(input("Digite o depósito: "))
+saldo += deposito
+print ("O novo saldo é:", saldo)
